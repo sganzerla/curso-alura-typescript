6379af2007c69815a2c3a1214e5c6e874ab261e7
sganzerla <you@example.com>
2019-10-08 00:01:59 -0300

    proteção contra scripts maliciosos

--- app/js/views/NegociacoesView.js
+++ app/js/views/NegociacoesView.js
@@ -35,7 +35,7 @@ System.register(["./View"], function (exports_1, context_1) {
 
             <tfoot>
             </tfoot>
-        </table> 
+        </table>
         `;
                 }
             };
