91ff5bb3f0ae018555f097569dcc29d020871123
sganzerla <you@example.com>
2019-10-20 09:48:30 -0300

    template table html dinamico

--- alurabank/app/js/controllers/NegociacaoController.js
+++ alurabank/app/js/controllers/NegociacaoController.js
@@ -1,13 +1,27 @@
 class NegociacaoController {
     constructor() {
+        this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoesView');
+        // document.querySelector busca elemento do DOM da página
+        // casting convertendo mais generico para mais específico
         this._inputData = document.querySelector('#data');
         this._inputQuantidade = document.querySelector('#quantidade');
         this._inputValor = document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
     adiciona(event) {
         // impedir o carregamento da página após o evento
         event.preventDefault();
-        const negociacao = new Negociacao(this._inputData.value, this._inputQuantidade.value, this._inputValor.value);
-        console.log(negociacao);
+        const negociacao = new Negociacao(
+        // converte formato string para date substituindo hífen pela vírgula
+        new Date(this._inputData.value.replace(/-/g, ',')), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
+        this._negociacoes.adiciona(negociacao);
+        this._negociacoesView.update(this._negociacoes);
+        // this._negociacoes.paraArray().forEach(negociacao => {
+        //     console.log(negociacao.data);
+        //     console.log(negociacao.quantidade);
+        //     console.log(negociacao.valor);
+        // });
+        // console.log(negociacao);
     }
 }
--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,8 +1,11 @@
+
 class NegociacaoController {
 
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor() {
         // document.querySelector busca elemento do DOM da página
@@ -10,6 +13,7 @@ class NegociacaoController {
         this._inputData = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event) {
@@ -23,6 +27,14 @@ class NegociacaoController {
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao);
+        this._negociacoesView.update(this._negociacoes);
+
+        // this._negociacoes.paraArray().forEach(negociacao => {
+        //     console.log(negociacao.data);
+        //     console.log(negociacao.quantidade);
+        //     console.log(negociacao.valor);
+        // });
+        // console.log(negociacao);
     }
 }
