80c537621c19da4344fa9211ec0d5317920b6e7e
sganzerla <you@example.com>
2019-10-20 00:23:33 -0300

    controller para manipular negociacoes

---
+++ alurabank/app/js/controllers/NegociacaoController.js
@@ -0,0 +1,13 @@
+class NegociacaoController {
+    constructor() {
+        this._inputData = document.querySelector('#data');
+        this._inputQuantidade = document.querySelector('#quantidade');
+        this._inputValor = document.querySelector('#valor');
+    }
+    adiciona(event) {
+        // impedir o carregamento da página após o evento
+        event.preventDefault();
+        const negociacao = new Negociacao(this._inputData.value, this._inputQuantidade.value, this._inputValor.value);
+        console.log(negociacao);
+    }
+}
