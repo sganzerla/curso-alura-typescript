1ec38fab9b1d0ffb56340599baba578e6de8f541
sganzerla <you@example.com>
2019-10-19 23:56:30 -0300

    noEmitOnError impede compilação projeto com erro no código

--- alurabank/app/js/models/Negociacao.js
+++ alurabank/app/js/models/Negociacao.js
@@ -1,14 +1,9 @@
 class Negociacao {
-    private _data;
-    private _quantidade;
-    private _valor;
-
     constructor(data, quantidade, valor) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
-
     get data() {
         return this._data;
     }
